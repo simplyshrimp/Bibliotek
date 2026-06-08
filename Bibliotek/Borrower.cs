b888d712a329fafc40ddd1f592f13d422529ab2e
simplyshrimp <rei@mlb.dk>
2026-06-08 08:46:17 +0200

    Add project files.

---
+++ Bibliotek/Borrower.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibliotek
+{
+    public class Borrower
+    {
+        public int BorrowerNumber { get; set; }
+        public string Library { get; set; }
+
+        public Borrower(int  BorrowerNumber, string Library)
+        {
+            this.BorrowerNumber = BorrowerNumber;
+            this.Library = Library;
+        }
+
+        public string GetBorrower()
+        {
+            return ($"ID: {BorrowerNumber}\nLibrary: {Library}");
+        }
+    }
+}
---
+++ Bibliotek/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Bibliotek
+{
+    internal class Program
+    {
+        static List<Borrower> borrowers = new();
+        static void Main(string[] args)
+        {
+            Borrower test = new(1, "Test Library");
+            Borrower test2 = new(2, "Test Library");
+            Borrower test3 = new(3, "test Library");
+            borrowers.Add(test);
+            borrowers.Add(test2);
+            borrowers.Add(test3);
+
+            Console.WriteLine(test.GetBorrower());
+        }
+        static void PrintBorrowers()
+        {
+            foreach(Borrower borrower in borrowers)
+            {
+                Console.WriteLine(borrower.GetBorrower());
+            }
+        }
+    }
+}
